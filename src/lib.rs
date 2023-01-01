15122a5b6a81e2bdae0e5c49e1fe54e95818af3d
Matthew J. Berger <matthewberger@nevada.unr.edu>
2022-12-31 17:45:01 -0700

    add error type

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,21 @@ use std::{
     rc::Rc,
 };
 
+pub type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
+#[derive(Debug)]
+pub struct EntityNotFoundError {
+    entity: Entity,
+}
+
+impl std::error::Error for EntityNotFoundError {}
+
+impl std::fmt::Display for EntityNotFoundError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Entity '{}' does not exist.", self.entity)
+    }
+}
+
 // Entities:                    Entity 0                       Entity 1   Entity 2                         Entity 3
 // Physics Components   -> Vec( Some(Physics { vel: 3 }),      None,      Some(Physics { vel: 10 }),       Some(Physics { vel: 04 }) )
 // Position Components  -> Vec( Some(Position { x: 3, y: 3 }), None,      Some(Position { x: 10, y: -2 }), Some(Position { x: 100, y: -20 }) )
@@ -45,10 +60,9 @@ impl World {
         entity
     }
 
-    pub fn add_component<T: 'static>(&mut self, entity: Entity, component: T) {
+    pub fn add_component<T: 'static>(&mut self, entity: Entity, component: T) -> Result<()> {
         if entity >= self.number_of_entities {
-            // TODO: make this an error
-            return;
+            return Err(Box::new(EntityNotFoundError { entity }) as Box<dyn std::error::Error>);
         }
         let mut components = self
             .components
@@ -56,14 +70,14 @@ impl World {
             .or_insert(Rc::new(RefCell::new(Vec::new())))
             .borrow_mut();
 
-        match components.get_mut(entity) {
+        Ok(match components.get_mut(entity) {
             Some(c) => {
                 *c = Some(Box::new(component));
             }
             None => {
                 components.insert(entity, Some(Box::new(component)));
             }
-        }
+        })
     }
 
     pub fn get_component<'a, T: 'static>(&self, entity: Entity) -> Option<Ref<T>> {
@@ -127,11 +141,11 @@ mod tests {
     }
 
     #[test]
-    fn add_component() {
+    fn add_component() -> Result<()> {
         let mut world = World::default();
         let entity = world.create_entity();
-        world.add_component(entity, Position::default());
-        world.add_component(entity, Health { value: 10 });
+        world.add_component(entity, Position::default())?;
+        world.add_component(entity, Health { value: 10 })?;
         world.get_component_mut::<Health>(entity).unwrap().value = 0;
         assert_eq!(
             *world.get_component::<Position>(entity).unwrap(),
@@ -141,6 +155,7 @@ mod tests {
             *world.get_component::<Health>(entity).unwrap(),
             Health { value: 0 }
         );
+        Ok(())
     }
 
     #[test]
@@ -166,11 +181,11 @@ mod tests {
     }
 
     #[test]
-    fn system() {
+    fn system() -> Result<()> {
         let mut world = World::default();
         let entity = world.create_entity();
-        world.add_component(entity, Position::default());
-        world.add_component(entity, Health { value: 10 });
+        world.add_component(entity, Position::default())?;
+        world.add_component(entity, Health { value: 10 })?;
 
         {
             let mut p = world
@@ -208,6 +223,8 @@ mod tests {
         let position: Option<&Position> = position_components
             .get(entity)
             .and_then(|v| v.as_ref().and_then(|c| c.downcast_ref()));
-        assert_eq!(position, Some(&Position { x: 10.0, y: 10.0 }))
+        assert_eq!(position, Some(&Position { x: 10.0, y: 10.0 }));
+
+        Ok(())
     }
 }
